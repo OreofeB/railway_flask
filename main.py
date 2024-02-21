da142154d88f6a5ab987e4b154677d984c08046e
OreofeB <115746859+OreofeB@users.noreply.github.com>
2024-02-21 17:04:02 +0100

    Update main.py
    changed to model 3

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ app = Flask(__name__)
 CORS(app)  
 
 # Load model
-# model_path = 'C:\\Users\\parkway\\Documents\\Python Code\\ML_Models\\LP_Models\\models\\dp_model_1.h5'
-model_path = 'dp_model.h5' 
+model_path = 'dp_model_3.h5' 
 model = tf.keras.models.load_model(model_path)
 
 # Preprocessing function
