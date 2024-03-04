d2ec73f2f8967ce769972d06ce8f27ce2011acb9
OreofeB <115746859+OreofeB@users.noreply.github.com>
2024-03-04 20:58:19 +0100

    Update main.py
    added 3 new models and added a standard scaler function

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from flask import Flask, request, jsonify
 from datetime import datetime as dt  
 import numpy as np
 import pandas as pd
-import pickle
 import tensorflow as tf
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from flask_cors import CORS  
 import os
 
@@ -12,8 +11,15 @@ app = Flask(__name__)
 CORS(app)  
 
 # Load model
-model_path = 'dp_model_6.h5' 
-model = tf.keras.models.load_model(model_path)
+model_path_1 = 'dp_model_6.h5' 
+model_1 = tf.keras.models.load_model(model_path_1)
+
+model_path_2 = 'dp_model_7.h5' 
+model_2 = tf.keras.models.load_model(model_path_2)
+
+model_path_3 = 'dp_model_3.h5' 
+model_3 = tf.keras.models.load_model(model_path_3)
+
 
 # Preprocessing function
 def preprocess_data(data_df):
@@ -54,6 +60,7 @@ def preprocess_data(data_df):
 
     new_data['proposed_payday'] = process_column(new_data['proposed_payday'])
 
+
     return new_data
 
 # Prediction route
@@ -66,27 +73,43 @@ def predict():
     
     # Preprocess
     preprocessed_data = preprocess_data(data_df)
+    sc = StandardScaler()
+    preprocessed_data = sc.fit_transform(preprocessed_data)
     loan_id = data_df['loan_id']
     loan_id = int(loan_id[0])
     
     # Make predictions using the model
-    predictions = model.predict(preprocessed_data.values)
+    predictions_1 = model_1.predict(preprocessed_data)
+    predictions_2 = model_2.predict(preprocessed_data)
+    predictions_3 = model_3.predict(preprocessed_data)
+
 
     # Adjust the prediction output format and Round the prediction to 0 or 1
-    rounded_prediction = int(np.round(predictions[0]))
-    predictions_percentage = predictions * 100
-
-    # Adjust the prediction output format and round the prediction to 0 or 1
-    rounded_percentage = np.round(float(predictions_percentage), decimals=2)
-
+    r_prediction_1 = rounded_prediction_1 = np.round(float(predictions_1[0]), decimals=6)
+    predictions_percentage_1 = predictions_1 * 100
+    r_percentage_1 = rounded_percentage_1 = np.round(float(predictions_percentage_1), decimals=2)
+    
+    r_prediction_2 = rounded_prediction_2 = np.round(float(predictions_2[0]), decimals=6)
+    predictions_percentage_2 = predictions_2 * 100
+    r_percentage_2 = rounded_percentage_2 = np.round(float(predictions_percentage_2), decimals=2)
+    
+    r_prediction_3 = rounded_prediction_3 = np.round(float(predictions_3[0]), decimals=6)
+    predictions_percentage_3 = predictions_3 * 100
+    r_percentage_3 = rounded_percentage_3 = np.round(float(predictions_percentage_3), decimals=2)
+    
     # Timestamp
     timestamp = dt.now().strftime("%Y-%m-%d %H:%M:%S")
 
+
     # Determine the result based on the rounded prediction
-    if rounded_prediction == 0:
-        result = {'Date': timestamp, 'Loan ID': loan_id, 'prediction': float(predictions), 'prediction (%)': rounded_percentage, 'rounded prediction': 0, 'status': 'Default'}
-    else:
-        result = {'Date': timestamp, 'Loan ID': loan_id, 'prediction': float(predictions), 'prediction (%)': rounded_percentage, 'rounded prediction': 1, 'status': 'Not Default'}
+    result = {'Date': timestamp 
+              ,'Loan ID': loan_id
+              ,'Prediction 1': (r_prediction_1)
+              ,'Prediction 1 (%)': (r_percentage_1)
+              ,'Prediction 2': (r_prediction_2)
+              ,'Prediction 2 (%)': (r_percentage_2)
+              ,'Prediction 3': (r_prediction_3)
+              ,'Prediction 3 (%)': (r_percentage_3)}
 
     return jsonify(result)
 
